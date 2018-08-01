4ef3bd0467c4a87b77231b0470f145c1f8974688
Andrés Tovar <salas@unilibre.edu.co>
2018-07-31 20:59:30 -0500

    completa

--- 7.PORFIN/app.js
+++ 7.PORFIN/app.js
@@ -1,22 +1,30 @@
 const http = require('http');
 const mate = require('./matematicas.js');
-http.get("http://jsonplaceholder.typicode.com/posts" , (resp) => {
-    let data = '';
-   
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-   
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(data);
-    });
-   
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
-console.log(mate);
-console.log(http);
+const fs = require('fs');
+const host = ''
+const Port = 3000;
 
-console.log(mate.sumar(2,3));
+let server = http.createServer((req,res)=>{
+
+    if(req.url === '/'){
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        });
+        res.end("<h1>BASURA</h1>");
+    }
+    else if(req.url === '/hola'){
+        fs.readFile('index.html','UTF-8',(err,data)=>{
+            if(err) {
+                res.writeHead(404);
+                res.end('<h1>404</h1>')
+            }else{
+                res.end(data);
+            }
+           
+        });
+    }
+    else{
+        res.end("<h1>BASURA 404</h1>");
+    }
+
+}).listen(Port, host, ()=>{console.log("escuchando "+Port)});
