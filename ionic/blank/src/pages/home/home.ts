7fdcb0738e2b09bb3ac57f04640c4e179f9e8ac0
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-08-28 21:14:18 -0500

    ionic

---
+++ ionic/blank/src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {ANIMALES} from '../../data/data.animales';
+import { Animal } from '../../intefaces/animal.interface';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  animals : Animal[] = [];  
+
+  constructor() {
+    this.animals = ANIMALES.slice(0)
+  }
+
+}
